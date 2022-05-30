cd375cc4f7893863560ce7f47c61e24cbb8f298f
Jose Miguel Navarrete Vera <71886137+JoseMiguelNV@users.noreply.github.com>
2022-05-30 11:44:35 +0200

    Roman numeral and Arabic numeral converter
    Exercise converting Roman numerals to Arabic numerals and vice versa.

---
+++ Romanos y decimales/Romanos y decimales/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Romanos_y_decimales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnConversion = new System.Windows.Forms.Button();
+            this.txb1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(255, 113);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(99, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Introduce un valor: ";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(276, 217);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(35, 13);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "label2";
+            // 
+            // btnConversion
+            // 
+            this.btnConversion.Location = new System.Drawing.Point(372, 300);
+            this.btnConversion.Name = "btnConversion";
+            this.btnConversion.Size = new System.Drawing.Size(75, 23);
+            this.btnConversion.TabIndex = 2;
+            this.btnConversion.Text = "Convertir";
+            this.btnConversion.UseVisualStyleBackColor = true;
+            this.btnConversion.Click += new System.EventHandler(this.btnConversion_Click);
+            // 
+            // txb1
+            // 
+            this.txb1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txb1.Location = new System.Drawing.Point(360, 110);
+            this.txb1.Name = "txb1";
+            this.txb1.Size = new System.Drawing.Size(100, 20);
+            this.txb1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txb1);
+            this.Controls.Add(this.btnConversion);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnConversion;
+        private System.Windows.Forms.TextBox txb1;
+    }
+}
+
---
+++ Romanos y decimales/Romanos y decimales/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Romanos_y_decimales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblResultado.Text = "";
+        }
+
+        private void btnConversion_Click(object sender, EventArgs e)
+        {
+            Regex regexRomano = new Regex("^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+            Regex regexArabigo = new Regex("^[0-9]{1,4}$");
+            if(regexRomano.IsMatch(txb1.Text))
+            {
+                lblResultado.Text = "En números decimales es: " + cadenaRomana(txb1.Text); 
+            }            
+            else if(regexArabigo.IsMatch(txb1.Text))
+            {
+                int.TryParse(txb1.Text, out int numDecimal);
+                lblResultado.Text = "En numeros romanos es: " + conversionDecimales(numDecimal);
+            }
+            else
+            {
+                lblResultado.Text = "Error en el parametro introducido.";
+            }
+        }
+
+        private int cadenaRomana(string numeroRomano)
+        {
+            int numero = 0;
+            foreach (char letra in numeroRomano)
+            {
+                numero = numero + obtenerLetra(letra);
+            }
+            if(numeroRomano.Contains("IV") || numeroRomano.Contains("IX"))
+            {
+                numero -= 2;
+            }
+            if (numeroRomano.Contains("Xl") || numeroRomano.Contains("XC"))
+            {
+                numero -= 20;
+            }
+            if (numeroRomano.Contains("CD") || numeroRomano.Contains("CM"))
+            {
+                numero -= 200;
+            }
+            return numero;
+        }
+
+        private int obtenerLetra(char letra)
+        {
+            switch(letra)
+            {
+                case 'M': return 1000; break;
+                case 'D': return 500; break;
+                case 'C': return 100; break;
+                case 'L': return 50; break;
+                case 'X': return 10; break;
+                case 'V': return 5; break;
+                case 'I': return 1; break;
+                default: return 0; break;
+            }
+        }
+
+        private string conversionDecimales(int numeroDecimal)
+        {
+            if(numeroDecimal > 3999)
+            {
+                lblResultado.Text = "No hay número superiores al 3999";
+            }
+            else if(numeroDecimal >= 1000)
+            {
+                return "M" + conversionDecimales(numeroDecimal - 1000);
+            }
+            else if (numeroDecimal >= 900)
+            {
+                return "CM" + conversionDecimales(numeroDecimal - 900);
+            }
+            else if (numeroDecimal >= 500)
+            {
+                return "D" + conversionDecimales(numeroDecimal - 500);
+            }
+            else if (numeroDecimal >= 400)
+            {
+                return "CD" + conversionDecimales(numeroDecimal - 400);
+            }
+            else if (numeroDecimal >= 100)
+            {
+                return "C" + conversionDecimales(numeroDecimal - 100);
+            }
+            else if (numeroDecimal >= 90)
+            {
+                return "XC" + conversionDecimales(numeroDecimal - 90);
+            }
+            else if (numeroDecimal >= 50)
+            {
+                return "L" + conversionDecimales(numeroDecimal - 50);
+            }
+            else if (numeroDecimal >= 40)
+            {
+                return "XL" + conversionDecimales(numeroDecimal - 40);
+            }
+            else if (numeroDecimal >= 10)
+            {
+                return "X" + conversionDecimales(numeroDecimal - 10);
+            }
+            else if (numeroDecimal >= 9)
+            {
+                return "IX" + conversionDecimales(numeroDecimal - 9);
+            }
+            else if (numeroDecimal >= 5)
+            {
+                return "V" + conversionDecimales(numeroDecimal - 5);
+            }
+            else if (numeroDecimal >= 4)
+            {
+                return "IV" + conversionDecimales(numeroDecimal - 4);
+            }
+            else if (numeroDecimal >= 1)
+            {
+                return "I" + conversionDecimales(numeroDecimal - 1);
+            }
+            else if(numeroDecimal < 1)
+            {
+                return "";
+            }
+            return "Error";
+        }
+    }
+}
