086ae89fd1745c56fecbcf7fbd044e0161fb6f56
Jose Miguel Navarrete Vera <71886137+JoseMiguelNV@users.noreply.github.com>
2022-05-30 10:57:52 +0200

    Filling Time Calculation Exercise

---
+++ Calculadora de tiempo de llenado/Calculadora de tiempo de llenado/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Calculadora_de_tiempo_de_llenado
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.lbl2);
+            this.groupBox1.Controls.Add(this.lbl1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(739, 197);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos para calcular el tiempo de llenado";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "cm3",
+            "dm3",
+            "m3"});
+            this.comboBox2.Location = new System.Drawing.Point(436, 90);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(153, 23);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "cm3/seg",
+            "dm3/seg",
+            "m3/seg"});
+            this.comboBox1.Location = new System.Drawing.Point(436, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(153, 23);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(313, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(313, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(133, 93);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(165, 15);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Tamaño del depósito de agua:";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(157, 51);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(141, 15);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Caudal de salida de agua:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn3);
+            this.groupBox2.Controls.Add(this.btn2);
+            this.groupBox2.Controls.Add(this.btn1);
+            this.groupBox2.Controls.Add(this.lbl3);
+            this.groupBox2.Location = new System.Drawing.Point(29, 224);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(738, 199);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tarea a realizar";
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(483, 121);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "Terminar";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(337, 121);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "Limpiar";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(196, 121);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Calcular";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(97, 53);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(26, 15);
+            this.lbl3.TabIndex = 0;
+            this.lbl3.Text = "lbl3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lbl3;
+    }
+}
---
+++ Calculadora de tiempo de llenado/Calculadora de tiempo de llenado/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_de_tiempo_de_llenado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbl3.Text = "";
+
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            double tiempoLlenadoDoble = 0;
+            int tiempoLlenadoEntero = (int)tiempoLlenadoDoble;
+            int horas = tiempoLlenadoEntero / 3600;
+            int minutos = (tiempoLlenadoEntero % 3600) / 60;
+            int segundos = (tiempoLlenadoEntero % 3600) % 60;
+            string caudal = comboBox1.SelectedItem.ToString();
+            string deposito = comboBox2.SelectedItem.ToString();
+            double numCaulal = Convert.ToDouble(textBox1.Text);
+            double tamanyoDeposito = Convert.ToDouble(textBox2.Text);
+  
+            switch (caudal)
+            {
+                case "cm3/seg":
+
+                    if(deposito == "cm3")
+                    {
+                        tiempoLlenadoDoble = tamanyoDeposito / numCaulal;
+                        tiempoLlenadoEntero = (int)tiempoLlenadoDoble;
+                        horas = tiempoLlenadoEntero / 3600;
+                        minutos = (tiempoLlenadoEntero % 3600) / 60;
+                        segundos = (tiempoLlenadoEntero % 3600) % 60;
+                    }
+                    else if(deposito == "dm3")
+                    {
+                        tiempoLlenadoDoble = (tamanyoDeposito * 1000) / numCaulal;
+                        tiempoLlenadoEntero = (int)tiempoLlenadoDoble;
+                        horas = tiempoLlenadoEntero / 3600;
+                        minutos = (tiempoLlenadoEntero % 3600) / 60;
+                        segundos = (tiempoLlenadoEntero % 3600) % 60;
+                    }
+                    else 
+                    {
+                        tiempoLlenadoDoble = (tamanyoDeposito * 1000000) / numCaulal;
+                        tiempoLlenadoEntero = (int)tiempoLlenadoDoble;
+                        horas = tiempoLlenadoEntero / 3600;
+                        minutos = (tiempoLlenadoEntero % 3600) / 60;
+                        segundos = (tiempoLlenadoEntero % 3600) % 60;
+                    }
+                    break;
+
+                case "dm3/seg":
+               
+                    if (deposito == "cm3")
+                    {
+                        tiempoLlenadoDoble = tamanyoDeposito / (numCaulal * 1000);
+                        tiempoLlenadoEntero = (int)tiempoLlenadoDoble;
+                        horas = tiempoLlenadoEntero / 3600;
+                        minutos = (tiempoLlenadoEntero % 3600) / 60;
+                        segundos = (tiempoLlenadoEntero % 3600) % 60;
+                    }
+                    else if (deposito == "dm3")
+                    {
+                        tiempoLlenadoDoble = tamanyoDeposito / numCaulal;
+                        tiempoLlenadoEntero = (int)tiempoLlenadoDoble;
+                        horas = tiempoLlenadoEntero / 3600;
+                        minutos = (tiempoLlenadoEntero % 3600) / 60;
+                        segundos = (tiempoLlenadoEntero % 3600) % 60;
+                    }
+                    else 
+                    {
+                        tiempoLlenadoDoble = (tamanyoDeposito * 1000) / numCaulal;
+                        tiempoLlenadoEntero = (int)tiempoLlenadoDoble;
+                        horas = tiempoLlenadoEntero / 3600;
+                        minutos = (tiempoLlenadoEntero % 3600) / 60;
+                        segundos = (tiempoLlenadoEntero % 3600) % 60;
+                    }
+                    break;
+
+                case "m3/seg":
+                   
+                    if (deposito == "cm3")
+                    {
+                        tiempoLlenadoDoble = tamanyoDeposito / (numCaulal * 1000000);
+                        tiempoLlenadoEntero = (int)tiempoLlenadoDoble;
+                        horas = tiempoLlenadoEntero / 3600;
+                        minutos = (tiempoLlenadoEntero % 3600) / 60;
+                        segundos = (tiempoLlenadoEntero % 3600) % 60;
+                    }
+                    else if (deposito == "dm3")
+                    {
+                        tiempoLlenadoDoble = tamanyoDeposito / (numCaulal * 1000);
+                        tiempoLlenadoEntero = (int)tiempoLlenadoDoble;
+                        horas = tiempoLlenadoEntero / 3600;
+                        minutos = (tiempoLlenadoEntero % 3600) / 60;
+                        segundos = (tiempoLlenadoEntero % 3600) % 60;
+                    }
+                    else
+                    {
+                        tiempoLlenadoDoble = tamanyoDeposito / numCaulal;
+                        tiempoLlenadoEntero = (int)tiempoLlenadoDoble;
+                        horas = tiempoLlenadoEntero / 3600;
+                        minutos = (tiempoLlenadoEntero % 3600) / 60;
+                        segundos = (tiempoLlenadoEntero % 3600) % 60;
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+            if (segundos >= 1)
+            {
+                lbl3.Text = "Tiempo estimado requerido para el llenado: " + horas + " horas, " + minutos + " minutos, y " + segundos + " segundos.";
+            }
+            else
+            {
+                lbl3.Text = "Tiempo estimado requerido para el llenado: Menos de un segundo.";
+            }
+            
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+            lbl3.Text = "";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
